bdf34aaf3c55f01878ccda8cd35b83d469d7503a
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-03-11 21:58:24 +0000

    code in index and js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
  */
 function doMathClicked() {
 
-  document.getElementById('add-math').innerHTML = '<p>6 + 5 = ' + (6 + 5) + '</p>'
-  document.getElementById('subtract-math').innerHTML = '<p>7 - 3 =  ' + (7 - 3) + '</p>'
+document.getElementById('area').innerHTML = '<p>Area = ' + (6 + 5) + '</p>'
+  document.getElementById('perimeter').innerHTML = '<p>Perimeter =  ' + ((5 + 3) * 2) + '</p>'
   document.getElementById('multiply-math').innerHTML = '<p>3 + 4 × 2 = ' + (3 + 4 * 2) + '</p>'
   document.getElementById('divide-math').innerHTML = '<p>(4 ÷ 2) + 3 = ' + ((4 / 2) + 3) + '</p>'
   document.getElementById('exponent-math').innerHTML = '<p>5 + 2³ = ' + (5 + 2 ** 3) + '</p>'

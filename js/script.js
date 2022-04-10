a84d2271fb56e64b60e079c66b9a3026dd0242d1
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-04-10 10:52:35 -0400

    edits

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@
 /**
  * Do basic math.
  */
-function doMathClicked() {
+ function doMathClicked() {
 
-document.getElementById('area').innerHTML = '<p>Area = ' + (6 + 5) + '</p>'
-  document.getElementById('perimeter').innerHTML = '<p>Perimeter =  ' + ((5 + 3) * 2) + '</p>'
-  document.getElementById('multiply-math').innerHTML = '<p>3 + 4 × 2 = ' + (3 + 4 * 2) + '</p>'
-  document.getElementById('divide-math').innerHTML = '<p>(4 ÷ 2) + 3 = ' + ((4 / 2) + 3) + '</p>'
-  document.getElementById('exponent-math').innerHTML = '<p>5 + 2³ = ' + (5 + 2 ** 3) + '</p>'
-}
+  document.getElementById('area').innerHTML = '<p>Area = ' + (6 + 5) + '</p>'
+    document.getElementById('perimeter').innerHTML = '<p>Perimeter =  ' + ((5 + 3) * 2) + '</p>'
+    document.getElementById('multiply-math').innerHTML = '<p>3 + 4 × 2 = ' + (3 + 4 * 2) + '</p>'
+    document.getElementById('divide-math').innerHTML = '<p>(4 ÷ 2) + 3 = ' + ((4 / 2) + 3) + '</p>'
+    document.getElementById('exponent-math').innerHTML = '<p>5 + 2³ = ' + (5 + 2 ** 3) + '</p>'
+  }
+  
